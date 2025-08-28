93fc1790beaac537a40ac8fa75ae6e95f618dcb8
Tomasz Bartlomowicz <bartlomowicz.tomasz@gmail.com>
2025-08-28 13:08:05 +0200

    Updated main_window and start_training window

--- Python_app/start_training.py
+++ Python_app/start_training.py
@@ -1,6 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QPushButton, QHBoxLayout, QVBoxLayout, QLabel, \
-    QSizePolicy, QTextEdit, QPlainTextEdit
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QScrollArea, QSizePolicy, QTextEdit, QPlainTextEdit
 from PyQt5.QtGui import QPainter, QLinearGradient, QColor, QIcon, QPixmap, QPen
 from PyQt5.QtCore import Qt, QSize, QTimer
 
@@ -11,13 +10,18 @@ import time
 class TrainingWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.is_paused = False
-        self.window_width = 1024
-        self.window_height = 600
-        self.setGeometry(100, 100, self.window_width, self.window_height)
 
-        # Declaring buttons and labels
-        self.label = QLabel("Choose training you want to start", self)
+        # Get used screen parameters
+        self.app = QApplication.instance()
+        self.screen = self.app.primaryScreen()
+        self.available_height = self.screen.availableGeometry().height()
+
+        # Declaring buttons
+
+        self.return_button = QPushButton()
+        self.return_button.clicked.connect(self.close)
+
+        # TODO Dynamic declaration of training buttons
         self.training1_label = QPushButton("Training 1")
         self.training2_label = QPushButton("Training 2")
         self.training3_label = QPushButton("Training 3")
@@ -27,50 +31,88 @@ class TrainingWindow(QWidget):
         self.buttons = [self.training1_label, self.training2_label, self.training3_label, self.training4_label,
                         self.training5_label, self.training6_label]
 
-        self.main_layout = QVBoxLayout()
+        self.background = QPixmap("icons/basen3.jpg")
+
+        # Managing scroll area
+        self.scroll_area = QScrollArea()
+        self.scroll_widget = QWidget()
+        self.scroll_area.setWidget(self.scroll_widget)
+        self.scroll_area.setWidgetResizable(True)
+
+        self.buttons_layout = QVBoxLayout(self.scroll_widget)
+        self.main_layout = QHBoxLayout()
         self.init_ui()
         self.layout()
 
 
     def paintEvent(self, event):
-
         painter = QPainter(self)
-
-        gradient = QLinearGradient(0, 0, 1024, 600)
-        gradient.setColorAt(0, QColor("#0f0f0f"))
-        gradient.setColorAt(0.3, QColor("#1a1919"))
-        gradient.setColorAt(0.6, QColor("#242323"))
-        gradient.setColorAt(1, QColor("#333333"))
-        painter.fillRect(self.rect(), gradient)
+        painter.drawPixmap(self.rect(), self.background)
 
 
     def init_ui(self):
-        self.label.setStyleSheet("color: white;"
-                                "font: 20px 'Segoe UI' bold;")
-
-
-
         for button in self.buttons:
-            button.setStyleSheet("background-color: rgba(0, 0, 0, 0.7);"
-                                 "font: 20px 'Segoe UI' bold;"
-                                 "color: white;")
+            button.setStyleSheet(""" 
+            QPushButton {
+                                 background-color: rgba(0, 0, 0, 0.8);
+                                 color: white;
+                                 font: 30px 'Segoe UI';
+                                 font-weight: bold;
+                                 border-radius: 15px;
+                                 }
+                                 
+            QPushButton:pressed {
+                                background-color: rgba(25, 25, 25, 0.8);
+                                /* Gdy wciśnięty, przesuwamy zawartość w dół i w prawo */
+                                padding-top: 3px;
+                                padding-left: 3px;
+    }
+            """
+            )
             button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+            button.setMinimumHeight(self.available_height // 6)
+
+            self.return_button.setIcon(QIcon("icons/return.png"))
+            self.return_button.setStyleSheet(""" 
+            QPushButton {
+                                 background-color: rgba(0, 0, 0, 0.8);
+                                 color: white;
+                                 font: 30px 'Segoe UI';
+                                 font-weight: bold;
+                                 border-radius: 15px;
+                                 }
+                                 
+            QPushButton:pressed {
+                                background-color: rgba(25, 25, 25, 0.8);
+                                padding-top: 3px;
+                                padding-left: 3px;
+    }
+            """)
+            self.return_button.setIconSize(QSize(80, 80))
+
+            self.scroll_area.setStyleSheet("background-color: transparent; border: none;")
 
 
 
     def layout(self):
-        self.main_layout.addWidget(self.label)
         for button in self.buttons:
-            self.main_layout.addWidget(button)
+            self.buttons_layout.addWidget(button)
+        self.buttons_layout.setAlignment(Qt.AlignCenter)
+
+
+        self.main_layout.addStretch(2)
+        self.main_layout.addWidget(self.scroll_area, stretch = 4)
+        self.main_layout.addStretch(1)
+        self.main_layout.addWidget(self.return_button)
+        self.main_layout.addStretch(1)
+
+
         self.setLayout(self.main_layout)
-
-
 
 def main():
     app = QApplication(sys.argv)
     window = TrainingWindow()
     window.showFullScreen()
-    window.resize(1024, 600)
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
